c27b8a0c112370017bb041588d909ec30e2be3d2
mdhasankha-1023 <mdhasankha.wd@gmail.com>
2023-11-18 21:05:56 +0600

    [ADD:] dotenv added and hide mongodb user and password

--- index.js
+++ index.js
@@ -1,9 +1,43 @@
 const express = require('express')
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config()
 const app = express();
 const port = process.env.PORT || 5000;
 
 // middleware
 // app.use(cors())
+
+
+// mongodb start
+
+const uri =`mongodb+srv://${process.env.SKB_DB_USER}:${process.env.SKB_DB_PASS}@cluster0.kgqmoa1.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    // await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.log);
+
+// mongodb end
+
+
 
 
 // test server
